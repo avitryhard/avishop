3c6a987c37771a4cfe946e52e0b725f573e2c076
DESKTOP-FLJKL2P\kings <kingswift0107@gmail.com>
2018-10-08 17:43:56 +0700

    create migrations and repository

---
+++ AviShop.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using AviShop.Data.Infrastructure;
+using AviShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AviShop.Data.Repositories
+{
+    public interface IProductRepository
+    {
+    }
+
+    public class ProductRepository : RepositoryBase<Product>
+    {
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
