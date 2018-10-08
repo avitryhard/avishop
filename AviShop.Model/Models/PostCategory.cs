03a4d197acc2572dd0ed0f37a0a2f7e0b70f1842
DESKTOP-FLJKL2P\kings <kingswift0107@gmail.com>
2018-10-08 14:48:44 +0700

    Add PostCategory to Models

--- AviShop.Model/Models/PostCategory.cs
+++ AviShop.Model/Models/PostCategory.cs
@@ -9,17 +9,25 @@ namespace AviShop.Model.Models
     public class PostCategory : Auditable
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
 
         [Required]
+        [MaxLength(256)]
         public string Name { get; set; }
 
-        [Required]
+        [Column(TypeName ="varchar")]
+        [MaxLength(256)]
+        [Required]        
         public string Alias { get; set; }
 
+        [MaxLength(500)]
         public string Description { get; set; }
-        public string ParentID { get; set; }
+
+        public int? ParentID { get; set; }
         public int DisplayOder { get; set; }
+
+        [MaxLength(256)]
         public string Image { get; set; }
 
         public bool? HomeFlag { get; set; }
