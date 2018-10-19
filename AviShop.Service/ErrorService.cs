e424325e6881878524f3ef3dcd533bbdef57e208
DESKTOP-FLJKL2P\kings <kingswift0107@gmail.com>
2018-10-19 21:24:57 +0700

    test link

--- AviShop.Service/ErrorService.cs
+++ AviShop.Service/ErrorService.cs
@@ -1,35 +1,37 @@
 ﻿using AviShop.Data.Infrastructure;
 using AviShop.Data.Repositories;
 using AviShop.Model.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AviShop.Service
 {
     public partial interface IErrorService
     {
         Error Create(Error error);
+
         void Save();
     }
+
     public class ErrorService : IErrorService
     {
         #region Properties
+
         private IErrorRepository _errorRepository;
         private IUnitOfWork _unitOfWork;
-        #endregion
+
+        #endregion Properties
 
         #region ctor
-        public ErrorService(ErrorRepository errorRepository,UnitOfWork unitOfWork)
+
+        public ErrorService(ErrorRepository errorRepository, UnitOfWork unitOfWork)
         {
             this._errorRepository = errorRepository;
             this._unitOfWork = unitOfWork;
         }
-        #endregion
+
+        #endregion ctor
 
         #region Function
+
         public Error Create(Error error)
         {
             return _errorRepository.Add(error);
@@ -39,6 +41,7 @@ namespace AviShop.Service
         {
             _unitOfWork.Commit();
         }
-        #endregion
+
+        #endregion Function
     }
 }
