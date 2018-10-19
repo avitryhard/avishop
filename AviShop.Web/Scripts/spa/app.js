8672ffd1aeb4a1573361d41438a13a5169cae814
DESKTOP-FLJKL2P\kings <kingswift0107@gmail.com>
2018-10-19 22:51:41 +0700

    Hoc AngularJS

---
+++ AviShop.Web/Scripts/spa/app.js
@@ -0,0 +1,20 @@
+﻿var myApp = angular.module('AviShop', []);
+
+myApp.controller("thongBaoTruong", thongBaoTruong);
+myApp.controller("thongBaoHocSinh", thongBaoHocSinh);
+myApp.controller("thongBaoGiaoVien", thongBaoGiaoVien);
+
+//declare
+function thongBaoTruong($scope)
+{
+    $scope.message = "Day la thong bao cua truong";        
+}
+
+function thongBaoHocSinh($scope)
+{
+    $scope.message = "Day la thong bao cua hoc sinh";
+}
+
+function thongBaoGiaoVien($scope) {
+    $scope.message = "Day la thong bao cua giao vien";
+}
